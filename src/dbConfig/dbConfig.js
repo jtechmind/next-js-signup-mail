f29b861b01c8addb8293386b0a276df3c559915d
jtechmind <jtechmind@gmail.com>
2024-03-22 14:31:58 +0530

    dbConfig file added for MongoDB Connection

---
+++ src/dbConfig/dbConfig.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export async function connectMongoDb() {
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("MongoDB Connected");
+    });
+    connection.on("error", (err) => {
+      console.log(
+        `MongoDB connection error please make sure DB is UP and Running ${err}`
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.error(`MongoDB connection failed` + err);
+  }
+}
